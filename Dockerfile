d35d5b84f1db328b4f72a386889937496cf2d3d5
Lassi Paavolainen <lassi.paavolainen@gmail.com>
2020-11-28 11:40:09 +0200

    Updated to new version of biaflows-utilities and cytomine-python-client

--- Dockerfile
+++ Dockerfile
@@ -40,20 +40,20 @@ RUN mkdir /app
 RUN cd /app && git clone https://github.com/CellProfiler/CellProfiler.git
 RUN cd /app/CellProfiler && git checkout 2.2.0 && python2.7 -m pip install --editable .
 
-# Install Python3.6
+# Install Python3.7
 RUN apt-get update -y && \
     apt-get install -y software-properties-common python-software-properties
 
 RUN add-apt-repository -y ppa:deadsnakes/ppa && \
     apt-get update -y && \
-    apt-get install -y python3.6 && \
-    apt-get install -y python3.6-dev && \
-    apt-get install -y python3.6-venv && \
+    apt-get install -y python3.7 && \
+    apt-get install -y python3.7-dev && \
+    apt-get install -y python3.7-venv && \
     apt-get install -y wget
 
 RUN cd /tmp && \
     wget https://bootstrap.pypa.io/get-pip.py && \
-    python3.6 get-pip.py
+    python3.7 get-pip.py
 
 RUN pip3 install requests \
     requests-toolbelt \
@@ -67,20 +67,20 @@ RUN pip3 install requests \
 RUN cd / && \
     git clone https://github.com/cytomine-uliege/Cytomine-python-client.git && \
     cd Cytomine-python-client && \
-    git checkout tags/v2.3.0.poc.1 && \
+    git checkout tags/v2.7.3 && \
     pip3 install .
 
 # Install NEUBIAS WG5 Utilities
 RUN apt-get update && apt-get install libgeos-dev -y && apt-get clean
-RUN git clone https://github.com/Neubias-WG5/neubiaswg5-utilities.git && \
-    cd /neubiaswg5-utilities/ && git checkout tags/v0.8.8 && pip install .
+RUN git clone https://github.com/Neubias-WG5/biaflows-utilities.git && \
+    cd /biaflows-utilities/ && git checkout tags/v0.9.1 && pip install .
 
-RUN chmod +x /neubiaswg5-utilities/bin/*
-RUN cp /neubiaswg5-utilities/bin/* /usr/bin/
-RUN rm -r /neubiaswg5-utilities
+RUN chmod +x /biaflows-utilities/bin/*
+RUN cp /biaflows-utilities/bin/* /usr/bin/
+RUN rm -r /biaflows-utilities
 
 ADD wrapper.py /app/wrapper.py
 ADD CP_detect_nuclei.cppipe /app/CP_detect_nuclei.cppipe
 ADD descriptor.json /app/descriptor.json
 
-ENTRYPOINT ["python3.6","/app/wrapper.py"]
+ENTRYPOINT ["python3.7","/app/wrapper.py"]
--- wrapper.py
+++ wrapper.py
@@ -2,13 +2,13 @@ import sys
 import os
 from cytomine.models import Job
 from subprocess import call
-from neubiaswg5 import CLASS_OBJSEG
-from neubiaswg5.helpers import NeubiasJob, prepare_data, upload_data, upload_metrics
+from biaflows import CLASS_OBJSEG
+from biaflows.helpers import BiaflowsJob, prepare_data, upload_data, upload_metrics
 
-def parseCPparam(nj, pipeline, tmpdir):
+def parseCPparam(bj, pipeline, tmpdir):
     """
     """
-    sp = nj.software.parameters
+    sp = bj.software.parameters
     cpparams = {}
     for param in sp:
         cpparams[param['humanName']] = param['name']
@@ -19,7 +19,7 @@ def parseCPparam(nj, pipeline, tmpdir):
     for line in rhdl:
         ar = line.split(":")
         if ar[0].strip() in cpparams.keys():
-            line = ar[0] + ":" + str(getattr(nj.parameters,cpparams[ar[0].strip()]))+"\n"
+            line = ar[0] + ":" + str(getattr(bj.parameters,cpparams[ar[0].strip()]))+"\n"
         whdl.write(line)
 
     return mod_pipeline
@@ -29,10 +29,10 @@ def main(argv):
     base_path = "{}".format(os.getenv("HOME")) # Mandatory for Singularity
     problem_cls = CLASS_OBJSEG
 
-    with NeubiasJob.from_cli(argv) as nj:
-        nj.job.update(status=Job.RUNNING, progress=0, statusComment="Initialisation...")
+    with BiaflowsJob.from_cli(argv) as bj:
+        bj.job.update(status=Job.RUNNING, progress=0, statusComment="Initialisation...")
         # 1. Prepare data for workflow
-        in_imgs, gt_imgs, in_path, gt_path, out_path, tmp_path = prepare_data(problem_cls, nj, is_2d=True, **nj.flags)
+        in_imgs, gt_imgs, in_path, gt_path, out_path, tmp_path = prepare_data(problem_cls, bj, is_2d=True, **bj.flags)
 
         plugindir = "/app/plugins"
         pipeline = "/app/CP_detect_nuclei.cppipe"
@@ -43,8 +43,8 @@ def main(argv):
         fh.close()
 
         # 2. Run CellProfiler pipeline
-        nj.job.update(progress=25, statusComment="Launching workflow...")
-        mod_pipeline = parseCPparam(nj, pipeline, tmp_path)
+        bj.job.update(progress=25, statusComment="Launching workflow...")
+        mod_pipeline = parseCPparam(bj, pipeline, tmp_path)
         
         shArgs = [
             "python", "/app/CellProfiler/CellProfiler.py", "-c", "-r", "-b", "--do-not-fetch", "-p", mod_pipeline,
@@ -54,20 +54,20 @@ def main(argv):
 
         if return_code != 0:
             err_desc = "Failed to execute the CellProfiler pipeline (return code: {})".format(return_code)
-            nj.job.update(progress=50, statusComment=err_desc)
+            bj.job.update(progress=50, statusComment=err_desc)
             raise ValueError(err_desc)
 
         # 3. Upload data to BIAFLOWS
-        upload_data(problem_cls, nj, in_imgs, out_path, **nj.flags, monitor_params={
+        upload_data(problem_cls, bj, in_imgs, out_path, **bj.flags, monitor_params={
             "start": 60, "end": 90, "period": 0.1,
             "prefix": "Extracting and uploading polygons from masks"})
         
         # 4. Compute and upload metrics
-        nj.job.update(progress=90, statusComment="Computing and uploading metrics...")
-        upload_metrics(problem_cls, nj, in_imgs, gt_path, out_path, tmp_path, **nj.flags)
+        bj.job.update(progress=90, statusComment="Computing and uploading metrics...")
+        upload_metrics(problem_cls, bj, in_imgs, gt_path, out_path, tmp_path, **bj.flags)
 
         # 5. Pipeline finished
-        nj.job.update(progress=100, status=Job.TERMINATED, status_comment="Finished.")
+        bj.job.update(progress=100, status=Job.TERMINATED, status_comment="Finished.")
 
 
 if __name__ == "__main__":
